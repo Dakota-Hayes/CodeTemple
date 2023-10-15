d65f5c23e973f6f1d5af28e5d43abc418b13a751
Dakota Hayes <dakotahayes@users.noreply.github.com>
2023-10-14 20:49:35 -0700

    update

---
+++ PYTHON/check.py
@@ -0,0 +1,9 @@
+def checker(days_left,code_wars_points,assignments_done,steps_done):
+    print("Steps per day",(184 - steps_done)/days_left)
+
+    print("Code wars points per day",(400 - code_wars_points)/days_left)
+    
+    print("Assignments per day",(37-assignments_done)/days_left)
+
+checker(143,57,7,95)
+
